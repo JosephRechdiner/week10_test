df3532b1c277075ae60fe7e7b2829fa273237bf7
JosephRechdiner <jrechdiner2002@gmail.com>
2026-02-05 12:10:18 +0200

    added yaml files and finished

--- app/dal.py
+++ app/dal.py
@@ -1,33 +1,35 @@
+def convert_id(employees):
+    for employee in employees:
+        employee["_id"] = str(employee["_id"])
+    return employees
+
 class DalManager:
 # 1
     @staticmethod
     def get_engineering_high_salary_employees(database):
         query = {
             "job_role.department": "Engineering",
-            "salary": {"gt": 65000}
+            "salary": {"$gt": 65000}
         }
         projection = {
-            "_id": 0,
             "employee_id": 1,
             "name": 1,
             "salary": 1
         }
         employees = list(database.collection.find(query, projection))
-        return employees
+        return convert_id(employees)
 
 # 2
     @staticmethod
     def get_employees_by_age_and_role(database):
         query = {
             "$and": [
-                {"age": {"gte": 30}}, {"age": {"lte": 45}},
+                {"age": {"$gte": 30}}, {"age": {"$lte": 45}},
                 {"$or": [{"job_role.title": "Engineer"}, {"job_role.title": "Specialist"}]}
             ]
         }
         employees = list(database.collection.find(query))
-        for employee in employees:
-            employee["_id"] = str(employee["_id"])
-        return employees
+        return convert_id(employees)
 
 # 3
     @staticmethod
@@ -35,8 +37,7 @@ class DalManager:
         query = {
             "job_role.department": {"$ne": "HR"}
             }
-        
-        employees = list(database.collection.find(query, sort=("years_at_company", -1 )).limit(7))
+        employees = list(database.collection.find(query).sort("years_at_company", -1).limit(7))
         for employee in employees:
             employee["_id"] = str(employee["_id"])
         return employees
@@ -51,29 +52,26 @@ class DalManager:
             ]
         }
         projection = {
-            "_id": 0,
             "employee_id": 1,
             "name": 1,
-            "salary": 1,
+            "age": 1,
             "years_at_company": 1
         }
         employees = list(database.collection.find(query, projection))
-        return employees
+        return convert_id(employees)
 
 # 5
     @staticmethod
     def get_managers_excluding_departments(database):
         query = {
-            {"$and":[
+            "$and":[
                 {"job_role.title": "Manager"},
-                {"job_role.department": {"$not": {"$in": ["Sales", "Marketing"]}}}
-            ]}
-        }
-
+                {"job_role.department": {"$ne": "Sales"}},
+                {"job_role.department": {"$ne": "Marketing"}}
+                ]
+            }
         employees = list(database.collection.find(query))
-        for employee in employees:
-            employee["_id"] = str(employee["_id"])
-        return employees
+        return convert_id(employees)
 
 # 6
     @staticmethod
@@ -82,16 +80,15 @@ class DalManager:
             "$and": [
                 {"age": {"$lt": 35}},
                 {"$or": [
-                    {"name": {"$regex": "^Nelson"}},
-                    {"name": {"$regex": "^Wright"}}
+                    {"name": {"$regex": "Nelson$"}},
+                    {"name": {"$regex": "Wright$"}}
                 ]}
             ]
         }
         projection = {
-            "_id": 0,
             "name": 1,
             "age": 1,
             "job_role.department": 1
         }
         employees = list(database.collection.find(query, projection))
-        return employees
+        return convert_id(employees)
